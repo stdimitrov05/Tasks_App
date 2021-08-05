6c638650dd680811d0252870ee6552e702a2d4a9
stdimitrov05 <stdimitrov8b@gmail.com>
2021-08-05 19:59:47 +0300

    Add buttons

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import {
   TouchableOpacity,
   Keyboard,
   ScrollView,
+  Button,
+  Linking,
 } from "react-native";
 import Task from "./components/Taks";
 
@@ -28,20 +30,37 @@ export default function App() {
   };
 
   return (
-
     <View style={styles.container}>
-   
       <ScrollView
         contentContainerStyle={{
           flexGrow: 1,
         }}
         keyboardShouldPersistTaps="handled"
       >
-    
         <View style={styles.tasksWrapper}>
+          <View>
+            <TouchableOpacity
+              style={styles.gitHub}
+              onPress={() => Linking.openURL("https://github.com/stdimitrov05")}
+              underlayColor="#fff"
+            >
+              <Text style={styles.githubText}> 💬 GitHub: St_d05 </Text>
+            </TouchableOpacity>
+          </View>
+          <View>
+            <TouchableOpacity
+              style={styles.Instagram}
+              onPress={() =>
+                Linking.openURL("https://www.instagram.com/st_d05/")
+              }
+              underlayColor="#fff"
+            >
+              <Text style={styles.instagramText}>Instagram: St_d05</Text>
+            </TouchableOpacity>
+          </View>
           <Text style={styles.sectionTitle}>Today's tasks</Text>
+          <Text style={styles.creator}>Creator St_d05</Text>
           <View style={styles.items}>
-          
             {taskItems.map((item, index) => {
               return (
                 <TouchableOpacity
@@ -56,7 +75,6 @@ export default function App() {
         </View>
       </ScrollView>
 
-    
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         style={styles.writeTaskWrapper}
@@ -74,25 +92,59 @@ export default function App() {
         </TouchableOpacity>
       </KeyboardAvoidingView>
     </View>
-    
   );
 }
 
-
 const styles = StyleSheet.create({
+  gitHub: {
+    left: -140,
+    position: "absolute",
+    margin:5,
+    marginLeft: 130,
+    marginRight: 90,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#fff",
+  },
+  githubText: {
+    color: "#fff",
+    textAlign: "center",
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  Instagram: {
+    left: 100,
+    margin: 5,
+    marginLeft: 130,
+    marginRight: 90,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#fff",
+  },
+  instagramText: {
+    color: "#fff",
+    textAlign: "center",
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  creator: {
+    textAlign: "center",
+    color: "#FAFAFA",
+  },
   container: {
     flex: 1,
     backgroundColor: "#ff4d4d",
   },
   tasksWrapper: {
-    paddingTop: 80,
+    paddingTop: 25,
     paddingHorizontal: 20,
   },
   sectionTitle: {
     fontSize: 24,
-    textAlign:"center",
-    fontFamily: 'Arial', 
+    textAlign: "center",
+    fontFamily: "Arial",
     fontWeight: "bold",
+    color: "#FAFAFA",
   },
   items: {
     marginTop: 30,
